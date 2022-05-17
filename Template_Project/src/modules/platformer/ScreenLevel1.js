d0e52d5ca98fe6cf7404b06e28c006ddbc2670be
Nguyễn Thái An <thaian229@gmail.com>
2022-05-17 19:31:54 +0700

    Restructuring Folders

---
+++ Template_Project/src/modules/platformer/ScreenLevel1.js
@@ -0,0 +1,29 @@
+/**
+ * Simple Platform Level 1
+ */
+
+var ScreenLevel1 = cc.Layer.extend({
+    _itemMenu: null,
+    _beginPos: 0,
+    isMouseDown: false,
+
+    ctor:function () {
+        this._super();
+        this.loadGui();
+    },
+    loadGui:function () {
+        this.removeAllChildren();
+        var size = cc.winSize;
+
+        var btnBack = gv.commonButton(100, 64, size.width - 70, 52,"Back");
+        this.addChild(btnBack);
+        btnBack.addClickEventListener(this.onSelectBack.bind(this));
+    },
+    onEnter:function () {
+        this._super();
+    },
+    onSelectBack:function(sender)
+    {
+        fr.view(ScreenMenu);
+    }
+});
