31b0daf316302d2d36c7273e363bba98dd21a189
Nguyễn Thái An <50405868+thaian229@users.noreply.github.com>
2022-05-17 11:49:45 +0700

    Start level 1 scene

---
+++ src/modules/platformer/ScreenLevel1.js
@@ -0,0 +1,15 @@
+/**
+    Simple Platform Level 1
+ */
+
+var ScreenLevel1 = cc.Layer.extend({
+    _itemMenu: null,
+    _beginPos: 0,
+    isMouseDown: false,
+
+    ctor:function () {
+        this._super();
+        var size = cc.director.getVisibleSize();
+        
+    }
+})
