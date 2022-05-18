38143c5d24c5edee41a6e03e933053fc634393c4
Nguyễn Thái An <thaian229@gmail.com>
2022-05-18 19:54:15 +0700

    Add sample empty scene for later use

---
+++ Playground_Project/src/modules/testjs/Scene5.js
@@ -0,0 +1,18 @@
+/**
+ * Created by AnNT21.
+ */
+
+const Scene5 = cc.Layer.extend({
+    _itemMenu: null,
+    _beginPos: 0,
+    isMouseDown: false,
+
+    ctor:function () {
+        this._super();
+        let size = cc.director.getVisibleSize();
+    },
+
+    onEnter:function () {
+        this._super();
+    }
+});
