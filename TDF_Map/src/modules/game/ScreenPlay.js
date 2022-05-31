811813326172f1dc68e03bb5217848dba33194a0
Nguyễn Thái An <50405868+thaian229@users.noreply.github.com>
2022-05-31 08:58:58 +0700

    init project TDF_Map

---
+++ TDF_Map/src/modules/game/ScreenPlay.js
@@ -0,0 +1,31 @@
+/**
+ * Simple Scene
+ */
+
+var ScreenPlay = cc.Layer.extend({
+    _itemMenu: null,
+    _beginPos: 0,
+    isMouseDown: false,
+
+    ctor: function () {
+        this._super();
+        this.loadGui();
+    },
+
+    loadGui: function () {
+        this.removeAllChildren();
+        var size = cc.winSize;
+
+        var btnBack = gv.commonButton(100, 64, size.width - 70, 52, "Back");
+        this.addChild(btnBack);
+        btnBack.addClickEventListener(this.onSelectBack.bind(this));
+    },
+
+    onEnter: function () {
+        this._super();
+    },
+
+    onSelectBack: function (sender) {
+        fr.view(ScreenMenu);
+    }
+});
